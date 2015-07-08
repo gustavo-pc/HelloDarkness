fd3eb0cd123c10ec7c517f2cb1ed8e061dd60097
Gustavo Pinheiro Correia <gugu_850@hotmail.com>
2015-07-08 08:33:07 -0300

    Removing NSStream Delegate from view Classes and removing old text chat stuff

--- Hello Darkness/ReceiveViewController.swift
+++ Hello Darkness/ReceiveViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import MultipeerConnectivity
 import AudioToolbox
 
-class ReceiveViewController: UIViewController, MCSessionDelegate , NSStreamDelegate {
+class ReceiveViewController: UIViewController, MCSessionDelegate {
     
     
     var myID: MCPeerID?
@@ -63,52 +63,30 @@ class ReceiveViewController: UIViewController, MCSessionDelegate , NSStreamDeleg
             println("Connecting to \(peerID.displayName)")
         case .NotConnected:
             println("Lost connection to \(peerID.displayName)")
-    }
-}
-
-func session(session: MCSession!, didReceiveData data: NSData!, fromPeer peerID: MCPeerID!) {
-    println("recebi \(data.length) bytes");
-    
-    var receivedBuffer = AudioBuffer(mNumberChannels: 1, mDataByteSize: UInt32(data.length), mData: UnsafeMutablePointer<Void>(data.bytes));
-    
-    player.enqueueBuffer(receivedBuffer);
-}
-
-func session(session: MCSession!, didReceiveStream stream: NSInputStream!, withName streamName: String!, fromPeer peerID: MCPeerID!) {
-    
-    println("recebi stream")
-    self.inputStream = stream
-    inputStream?.delegate = self
-    inputStream?.scheduleInRunLoop(NSRunLoop.mainRunLoop(), forMode: NSDefaultRunLoopMode)
-    inputStream?.open()
-}
-
-func session(session: MCSession!, didStartReceivingResourceWithName resourceName: String!, fromPeer peerID: MCPeerID!, withProgress progress: NSProgress!) {
-}
-
-func session(session: MCSession!, didFinishReceivingResourceWithName resourceName: String!, fromPeer peerID: MCPeerID!, atURL localURL: NSURL!, withError error: NSError!) {
-}
-
-//MARK: NSStream Delegate
-
-func stream(aStream: NSStream, handleEvent eventCode: NSStreamEvent) {
-    switch eventCode {
-    case NSStreamEvent.HasBytesAvailable:
-        let input = aStream as! NSInputStream
-        var buffer = Array<UInt8>(count: 1024, repeatedValue: 0)
-        let numberBytes = input.read(&buffer, maxLength: 1024)
-        let dataString = NSData(bytes: &buffer, length: numberBytes)
-        let message = NSKeyedUnarchiver.unarchiveObjectWithData(dataString) as! String
-        println(message)
-        
-        //            var receivedData = UnsafeMutablePointer<UInt8>();
-        //            input.read(receivedData, maxLength: 2048);
-        
-        
-    default:
-        break
+        }
     }
     
+    func session(session: MCSession!, didReceiveData data: NSData!, fromPeer peerID: MCPeerID!) {
+//        println("recebi \(data.length) bytes");
+        
+        var receivedBuffer = AudioBuffer(mNumberChannels: 1, mDataByteSize: UInt32(data.length), mData: UnsafeMutablePointer<Void>(data.bytes));
+        
+        player.enqueueBuffer(receivedBuffer);
+    }
     
-}
+    func session(session: MCSession!, didReceiveStream stream: NSInputStream!, withName streamName: String!, fromPeer peerID: MCPeerID!) {
+        
+        println("recebi stream")
+        player.inputStream = stream
+        inputStream?.delegate = self.player
+        inputStream?.scheduleInRunLoop(NSRunLoop.mainRunLoop(), forMode: NSDefaultRunLoopMode)
+        inputStream?.open()
+    }
+    
+    func session(session: MCSession!, didStartReceivingResourceWithName resourceName: String!, fromPeer peerID: MCPeerID!, withProgress progress: NSProgress!) {
+    }
+    
+    func session(session: MCSession!, didFinishReceivingResourceWithName resourceName: String!, fromPeer peerID: MCPeerID!, atURL localURL: NSURL!, withError error: NSError!) {
+    }
+
 }
--- Hello Darkness/SendViewController.swift
+++ Hello Darkness/SendViewController.swift
@@ -18,14 +18,6 @@ class SendViewController: UIViewController, MCNearbyServiceBrowserDelegate, MCSe
     
     let recorder = AudioStreamRecorder();
     
-    @IBOutlet weak var messageTextField: UITextField!
-    @IBOutlet weak var sendButton: UIButton!
-    
-    @IBAction func sendMessage() {
-//        let textData = NSKeyedArchiver.archivedDataWithRootObject(messageTextField.text)
-//        self.outputStream?.write(UnsafePointer(textData.bytes), maxLength: textData.length)
-    }
-    
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated);
         
@@ -62,95 +54,33 @@ class SendViewController: UIViewController, MCNearbyServiceBrowserDelegate, MCSe
     
     //MARK: AudioStreamRecorderDelegate
     func audioStreamRecorder(recorder: AudioStreamRecorder!, didRenderNewBuffer buffer: AudioBuffer) {
-//        println("chegou buffer novo, tamanho \(buffer.mDataByteSize)");
         
         if (mySession?.connectedPeers.count != 0) {
             
-            var err: NSError?;
-            
             let bufferData = NSData(bytes: buffer.mData, length: Int(buffer.mDataByteSize));
-            mySession?.sendData(bufferData, toPeers: mySession?.connectedPeers, withMode: .Reliable, error: &err);
+            mySession?.sendData(bufferData, toPeers: mySession?.connectedPeers, withMode: .Unreliable, error: nil);
             
-//            outputStream?.write(UnsafePointer(buffer.mData), maxLength: Int(buffer.mDataByteSize));
         }
         
         free(buffer.mData);
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    //MARK: Browser Delegate
-    func browser(browser: MCNearbyServiceBrowser!, foundPeer peerID: MCPeerID!, withDiscoveryInfo info: [NSObject : AnyObject]!) {
-        println("\(peerID.displayName) found")
-        browser.invitePeer(peerID, toSession: mySession, withContext: nil, timeout: 0)
-    }
-    
-    func browser(browser: MCNearbyServiceBrowser!, lostPeer peerID: MCPeerID!) {
     }
     
     //MARK: Session Delegate
     func session(session: MCSession!, peer peerID: MCPeerID!, didChangeState state: MCSessionState) {
         switch state {
         case .Connected:
-            dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                self.sendButton.enabled = true
-            })
-            var err: NSError?
-            
-//            self.outputStream = mySession?.startStreamWithName("stream", toPeer: peerID, error: &err)
-//            if let outputStream = outputStream {
-//                outputStream.delegate = self
-//                outputStream.scheduleInRunLoop(NSRunLoop.mainRunLoop(), forMode: NSDefaultRunLoopMode)
-//                outputStream.open()
-//            }
+            self.recorder.outputStream = mySession?.startStreamWithName("stream", toPeer: peerID, error: nil)
+            if let outputStream = recorder.outputStream {
+//                outputStream.delegate = recorder
+                outputStream.scheduleInRunLoop(NSRunLoop.mainRunLoop(), forMode: NSDefaultRunLoopMode)
+                outputStream.open()
+            }
             
         case .Connecting:
             println("Connecting to \(peerID.displayName)")
         case .NotConnected:
             println("Lost connection to \(peerID.displayName)")
-            dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                self.sendButton.enabled = false
-            })        }
+        }
     }
     
     func session(session: MCSession!, didReceiveData data: NSData!, fromPeer peerID: MCPeerID!) {
@@ -158,25 +88,18 @@ class SendViewController: UIViewController, MCNearbyServiceBrowserDelegate, MCSe
     }
     
     func session(session: MCSession!, didReceiveStream stream: NSInputStream!, withName streamName: String!, fromPeer peerID: MCPeerID!) {
-        
     }
     
-    func session(session: MCSession!, didStartReceivingResourceWithName resourceName: String!, fromPeer peerID: MCPeerID!, withProgress progress: NSProgress!) {
-        
+    func session(session: MCSession!, didStartReceivingResourceWithName resourceName: String!, fromPeer peerID: MCPeerID!, withProgress progress: NSProgress!) {}
+    
+    func session(session: MCSession!, didFinishReceivingResourceWithName resourceName: String!, fromPeer peerID: MCPeerID!, atURL localURL: NSURL!, withError error: NSError!) {}
+    
+    //MARK: Browser Delegate
+    func browser(browser: MCNearbyServiceBrowser!, foundPeer peerID: MCPeerID!, withDiscoveryInfo info: [NSObject : AnyObject]!) {
+        println("\(peerID.displayName) found")
+        browser.invitePeer(peerID, toSession: mySession, withContext: nil, timeout: 0)
     }
     
-    func session(session: MCSession!, didFinishReceivingResourceWithName resourceName: String!, fromPeer peerID: MCPeerID!, atURL localURL: NSURL!, withError error: NSError!) {
-        
-    }
-    
-    
-    //MARK: BrowserVC delegate
-    func browserViewControllerDidFinish(browserViewController: MCBrowserViewController!) {
-        //show10
-    }
-    
-    func browserViewControllerWasCancelled(browserViewController: MCBrowserViewController!) {
-        //massa cara
-        browserViewController.dismissViewControllerAnimated(true, completion: nil);
-    }
+    func browser(browser: MCNearbyServiceBrowser!, lostPeer peerID: MCPeerID!) {}
+
 }
